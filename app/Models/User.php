f2367815c762002a0bb0fb61efa8cdea057e4ca5
Limzy71 <muhikhsanjr05@gmail.com>
2025-12-14 14:36:47 +0700

    fix bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      */
 
     protected $table = 'users';
-    protected $primaryKey = 'id_user';
+    protected $primaryKey = 'id';
 
     protected $fillable = [
         'nama_lengkap',
